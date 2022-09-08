e0765af2ccb22373a48c9ccbb0779ce3bea6c2a0
GitTrey <109688333+GitTrey@users.noreply.github.com>
2022-09-07 21:45:02 -0500

    Add files via upload

---
+++ passwd_generator.py
@@ -0,0 +1,14 @@
+import random
+
+char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(>)<"
+
+while 1:
+    password_len = int(
+        input("what length would you like your password to be : "))
+    password_count = int(input("How many passwords would you like : "))
+    for x in range(0, password_count):
+        password = ""
+        for x in range(0, password_len):
+            password_char = random.choice(char)
+            password = password + password_char
+        print("Here is your password : ", password)
